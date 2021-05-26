be6b6860c22756535d60ee6c6bbaef2e37b068c0
Alex Blinov <cspp43@yandex.ru>
2021-05-26 23:15:45 +0300

    feature: карточки из js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
-/**
- * open/close popup
- */
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 let popup = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
@@ -11,6 +35,9 @@ let jobInput = formElement.querySelector('#status');
 
 let btnOpenPopup = document.querySelector('.profile__btn-edit');
 let btnClosePopup = document.querySelector('.popup__close');
+
+const cards = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#card').content;
 
 function openPopup(){
   popup.classList.add('popup_opened');
@@ -45,3 +72,14 @@ function formSubmitHandler (evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 btnOpenPopup.addEventListener('click', openPopup);
 btnClosePopup.addEventListener('click', closePopup);
+
+/**
+ * add cards on page
+ */
+initialCards.forEach((item) => {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__img').src = item.link;
+  cardElement.querySelector('.card__img').alt = item.name;
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cards.append(cardElement);
+});
