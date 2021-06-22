2ac1b69e7521598b0697d7db8ddd97b8f0ee9d34
Alex Blinov <cspp43@yandex.ru>
2021-06-23 01:18:44 +0300

    feature: add class FormValidator

--- js/index.js
+++ js/index.js
@@ -1,5 +1,41 @@
-import {initialCards} from './initial-сards.js';
-import {Card} from './card.js';
+import {Card} from './Card.js';
+import {FormValidator} from './FormValidator.js';
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const config = {
+  formSelector: '.form',
+  inputSelector: '.form__item',
+  submitButtonSelector: '.form__button',
+  inactiveButtonClass: 'form__button_inactive',
+  inputErrorClass: 'form__item_type_error',
+  errorClass: 'form__item-error_active'
+};
 
 // profile text fields
 const profileName = document.querySelector('.profile__name');
@@ -133,6 +169,17 @@ initialCards.forEach((card) => {
 
 // fill fields form "edit profile"
 fillFieldFormEdit();
+
+//validate forms
+const enableValidationForms = (config) => {
+  const formList = Array.from(document.querySelectorAll(config.formSelector));
+  formList.forEach((formElement) => {
+    const FormValid = new FormValidator(config, formElement);
+    return FormValid.enableValidation();
+  });
+};
+
+enableValidationForms(config);
 
 // submit forms
 formEditElement.addEventListener('submit', handleProfileFormSubmit);
